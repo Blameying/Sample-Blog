d57232069996d406ab3bf4613a48e31843a0ebbe
Blame <672804131@qq.com>
2018-07-02 11:04:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 CSRF_ENABLED  = True
-SECRET_KEY = "\x1a\xaf\xe4\xe8\x81536\xd4m\x8a\xd4GeK\xf3\x8f\xda[\xdc\x95\xd2rV"
+SECRET_KEY = ""
